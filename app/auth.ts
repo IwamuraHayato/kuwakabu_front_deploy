67b570ed43ecdb53142e08dfa7b72371da46ecad
Hayato Iwamura <8810.com@gmail.com>
2024-12-14 22:38:10 +0900

    mypage ログイン修正

--- app/auth.ts
+++ app/auth.ts
@@ -3,12 +3,41 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
+import Credentials from "next-auth/providers/credentials";
+import getUserFromDb from './login/getUserFromDb';
 
 export const { handlers, signIn, signOut } = NextAuth({
   providers: [
     GitHub({
       clientId: process.env.AUTH_GITHUB_ID, 
       clientSecret: process.env.AUTH_GITHUB_SECRET, 
+    }),
+    Credentials({
+      credentials: {
+        id: { label: "ID", type: "text" }, 
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        // 入力されたユーザー名とパスワードのチェック
+        if (!credentials?.id || !credentials?.password) {
+          throw new Error("id and password are required");
+        }
+    
+        try {    
+          // ユーザー名でユーザーを検索
+          const user = await getUserFromDb(credentials.id, credentials.password);
+    
+          if (!user) {
+            throw new Error("Invalid id or password");
+          }
+    
+          // 認証が成功した場合、ユーザーオブジェクトを返す
+          return { id: user.id, name: user.name};
+        } catch (error) {
+          console.error("Error in authorize:", error);
+          throw new Error("Authentication failed");
+        }
+      },
     }),
   ],
   callbacks: {
