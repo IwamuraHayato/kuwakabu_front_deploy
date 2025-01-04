b3617a5eb70bac34c3aeef3b28071b95f5ee6d11
Hayato Iwamura <8810.com@gmail.com>
2025-01-05 00:53:50 +0900

    mypageの画像表示エラーの対応を行いました。

--- next.config.js
+++ next.config.js
@@ -1,19 +1,32 @@
 const nextConfig = {
-    reactStrictMode: true,
-    output: `standalone`,
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'http',
-          hostname: '127.0.0.1',
-          port: '5000',
-          pathname: '/static/images/**',
-        },
-      ],
-      domains: process.env.NODE_ENV === 'development'
-            ? ['127.0.0.1'] // 開発環境
-            : ['tech0-gen-8-step3-app-py-16.azurewebsites.net'], // 本番環境
-    },
-  };
-  
-  module.exports = nextConfig;
+  reactStrictMode: true,
+  output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '5000',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5000',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tech0-gen-8-step3-app-py-16.azurewebsites.net',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'kuwakabu-backend-deploy.onrender.com',
+        pathname: '/images/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
